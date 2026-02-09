53db30c4548b5998a3ef51c507ff74e397b62c64
Jamin Chung <jamin.chung@ufl.edu>
2026-02-09 17:05:12 -0500

    added preliminary header stuff

--- emaan-valentines/src/App.jsx
+++ emaan-valentines/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/App.css'
+import { Header } from './components/header'
 import { FallingHearts } from './components/falling-hearts'
 import { HeroSection } from './components/hero-section'
 import { CoupleScene } from './components/couple-scene'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className="relative min-h-screen">
+      <Header/>
       <FallingHearts/>
       <div className="relative z-10">
         <HeroSection/>
---
+++ emaan-valentines/src/components/header.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+export function Header() {
+    return (
+        <div className="absolute top-0 left-0 w-full h-10 bg-black z-20">
+        </div>
+    )
+}
