80a4551d5949c49e144352d98a106aa6cb44d6e2
Jamin Chung <jamin.chung@ufl.edu>
2026-02-10 19:24:56 -0500

    setting up secrets | checkpoint commit

--- emaan-valentines/client/src/components/header.tsx
+++ emaan-valentines/client/src/components/header.tsx
@@ -1,8 +1,17 @@
 "use client"
 
+import { Link } from 'react-router-dom'
+
 export function Header() {
     return (
-        <div className="absolute top-0 left-0 w-full h-10 bg-black z-20">
+        <div className="absolute top-0 left-0 w-full h-10 bg-transparent z-20"> {/* header div */}
+            <Link to="/home" className="px-10 font-mono relative inline-block text-gray-600 after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-pink-500 after:transition-all after:duration-300 hover:after:w-full">
+                Valentine's Day!!!
+            </Link>
+
+            <Link to="/wall" className="px-10 font-mono relative inline-block text-gray-600 after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-pink-500 after:transition-all after:duration-300 hover:after:w-full">
+                Wall of Grievances 
+            </Link>
         </div>
     )
 }
---
+++ emaan-valentines/client/src/pages/home.tsx
@@ -0,0 +1,20 @@
+import '../styles/App.css'
+import { Header } from '../components/header'
+import { FallingHearts } from '../components/falling-hearts'
+import { HeroSection } from '../components/hero-section'
+import { CoupleScene } from '../components/couple-scene'
+
+
+export function Home() {
+
+    return (
+        <main className="relative min-h-screen">
+        <Header/>
+        <FallingHearts/>
+        <div className="relative z-10">
+            <HeroSection/>
+            <CoupleScene/>
+        </div>
+        </main>
+  )
+}
