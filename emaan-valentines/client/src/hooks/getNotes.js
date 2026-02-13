9261d4538b876632dafcd612554f50e0f31e5c77
Jamin Chung <jamin.chung@ufl.edu>
2026-02-12 21:06:40 -0500

    test 5

--- emaan-valentines/client/src/hooks/getNotes.js
+++ emaan-valentines/client/src/hooks/getNotes.js
@@ -3,12 +3,12 @@ import { backend_url } from '../constants/urls';
 async function getNotes() {
     try {
         const res = await fetch(`${backend_url}/grievances`, {
-            method: "GET",
+            method: "POST",
             credentials: "include",
             headers: {
                 "Content-Type": "application/json"
-            }
-
+            },
+            body: JSON.stringify({token: localStorage.getItem('auth_token')})
         });
         const data = await res.json()
 
